279b285b6fc4517e74fe464f91b00809a7c15730
BubblegumCombo <BubblegumCombo@users.noreply.github.com>
2021-06-08 13:28:36 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Big chungus
+        </p>
+        <a
+          className="App-link"
+          href="https://pornhub.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Big chungus but underlined in blue 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
